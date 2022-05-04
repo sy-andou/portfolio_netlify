61334f9501f1015aa1dd0e24a061398ef6146b46
sy-andou <kekeru.the.solo@gmail.com>
2022-05-04 11:14:16 +0900

    改行修正

--- store/works.js
+++ store/works.js
@@ -15,7 +15,7 @@ export const state=()=>({
             "title": "RSS",
             "kinds": "personal",
             "images":["/img/works/RSS/RSS1.png","/img/works/RSS/RSS2.png","/img/works/RSS/RSS3.png","/img/works/RSS/RSS4.png","/img/works/RSS/RSS5.png"],
-            "detail": "レストランの予約管理アプリケーションになります。検索・お気に入り登録・予約・レビュー投稿を行うことができます。また一般利用者以外の店舗代表者や管理者用の管理機能も作成しました。とくにこだわった点は、同一店舗かつ同一日時の既予約から残り予約可能人数を自動計算し、それ以上の人数は予約ができないようにしております。<br>サンプルアカウント<br>【管理者】email:sample@administrators.com, password:password<br>【店舗代表者】email:sample@representatives.com, password:password<br>【一般ユーザ】email:userA@users.com, password:password",
+            "detail": 'レストランの予約管理アプリケーションになります。検索・お気に入り登録・予約・レビュー投稿を行うことができます。また一般利用者以外の店舗代表者や管理者用の管理機能も作成しました。とくにこだわった点は、同一店舗かつ同一日時の既予約から残り予約可能人数を自動計算し、それ以上の人数は予約ができないようにしております。<br>サンプルアカウント<br>【管理者】email:sample@administrators.com, password:password<br>【店舗代表者】email:sample@representatives.com, password:password<br>【一般ユーザ】email:userA@users.com, password:password',
             "URL": "https://rss-syand.netlify.app",
             "technologies":["Nuxt.js","Laravel","Netlify","Heroku","AWS(s3)",],
             "github_1": "https://github.com/sy-andou/Rese_Netlify",
